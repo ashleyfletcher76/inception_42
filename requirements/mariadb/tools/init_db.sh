213c38eb6e7e0d71c0415860a2a653a29b65cc08
Ashley Fletcher <asfletch@2-I-5.42heilbronn.de>
2024-09-10 15:16:05 +0200

    Back to bare bones design. Back to scratch

--- requirements/mariadb/tools/init_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Load passwords from secrets
-MYSQL_PASSWORD=$(cat /run/secrets/mysql_password)
-MYSQL_ADMIN_PASSWORD=$(cat /run/secrets/mysql_admin_password)
-
-# Substituting environment variables into SQL commands
-envsubst < "/mnt/init.sql.template" > "/mnt/init.sql"
-mysql < "/mnt/init.sql"
