06f64bb7719180fd31b10c8a54dcfd354e6c4b02
Ashley Fletcher <asfletch@2-I-5.42heilbronn.de>
2024-09-10 15:48:03 +0200

    Corrected typo in dockerfile for mariadb

--- requirements/mariadb/tools/script.sh
+++ requirements/mariadb/tools/script.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-mysql_install_db
-mysqld
+mysql_install_db --user=mysql --datadir=/var/lib/mysql
+mysqld --user=mysql --init-file=/etc/mysql/init.sql
--- requirements/wordpress/tools/init_wordpress.sh
+++ requirements/wordpress/tools/init_wordpress.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 cd /var/www/html
+
+# Download the WordPress CLI
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
 
+# Download WordPress core files
+./wp-cli.phar core download --allow-root
+
+# Create wp-config.php using environment variables from the .env file
+./wp-cli.phar config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=mariadb --allow-root
+
+# Install WordPress using environment variables for admin credentials
+./wp-cli.phar core install --url=localhost --title=inception --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL} --allow-root
+
+# Start PHP-FPM in the foreground
 php-fpm8.2 -F
