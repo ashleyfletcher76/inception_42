9be40c2bbbeb590275d5afd49ab2629493051c91
Ashley Fletcher <asfletch@2-I-5.42heilbronn.de>
2024-09-10 15:04:47 +0200

    Removing some of the more complex details. Go back to basics again

--- requirements/mariadb/tools/init_db.sh
+++ requirements/mariadb/tools/init_db.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+# Load passwords from secrets
+MYSQL_PASSWORD=$(cat /run/secrets/mysql_password)
+MYSQL_ADMIN_PASSWORD=$(cat /run/secrets/mysql_admin_password)
 
+# Substituting environment variables into SQL commands
 envsubst < "/mnt/init.sql.template" > "/mnt/init.sql"
-
-mysql < /mnt/init.sql
+mysql < "/mnt/init.sql"
