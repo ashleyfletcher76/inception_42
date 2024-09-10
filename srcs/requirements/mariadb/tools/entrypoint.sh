ca838694efb28dc6136b11d6d379109c1f49a75e
Ashley Fletcher <asfletch@2-I-5.42heilbronn.de>
2024-09-10 14:48:13 +0200

    Removed the old version. Now reorganise directories

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-service mysql start
-
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
-	mysql < /docker-entrypoint-initdb.d/init.sql # create database if need be
-fi
-
-mysql_safe
--- srcs/requirements/nginx/tools/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-envsubst '{DOMAIN_NAME}' < /etc/nginx.template.conf > /etc/nginx/nginx.conf
-
-# start nginx
-nginx -g 'daemon off;'
